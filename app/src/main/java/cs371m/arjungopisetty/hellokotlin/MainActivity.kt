effe54c99ffc1d4def05e46257624cf2280846b7
arjungopisetty <arjun.gopisetty@gmail.com>
2019-06-23 13:49:12 -0700

    Integrate Fabric #11

--- app/src/main/java/cs371m/arjungopisetty/hellokotlin/MainActivity.kt
+++ app/src/main/java/cs371m/arjungopisetty/hellokotlin/MainActivity.kt
@@ -1,13 +1,16 @@
 package cs371m.arjungopisetty.hellokotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
 
         Timber.i("Hello World!")
