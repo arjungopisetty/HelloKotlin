92af51b5a27289eaf2ea608a974562e9fa5b53cc
arjungopisetty <arjun.gopisetty@gmail.com>
2019-06-23 01:05:20 -0700

    Migrate to AndroidX

--- app/src/main/java/cs371m/arjungopisetty/hellokotlin/MainActivity.kt
+++ app/src/main/java/cs371m/arjungopisetty/hellokotlin/MainActivity.kt
@@ -1,6 +1,6 @@
 package cs371m.arjungopisetty.hellokotlin
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
